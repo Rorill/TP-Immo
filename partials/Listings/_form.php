80af0753954e95934bed869fc991be7fadc14cd4
Guillaume Herold <guillaumeherold@gmail.com>
2024-04-16 11:59:31 +0200

    form creation

---
+++ partials/Listings/_form.php
@@ -0,0 +1,17 @@
+<form action="" method="post">
+
+<label for="name">Your name :</label>
+<input type="text" name="name"></input>
+
+<label for="mail">Your email :</label>
+<input type="mail" name="mail"></input>
+
+<label for="ListingType">Listing Type :</label>
+<select name="ListingType" id="ListingType">
+<option value="Rent">Rent</option>
+<option value="Sale">Sale</option>
+</select>
+<label for="Price">Price :</label>
+<input type="" name="Price">
+
+</form>
