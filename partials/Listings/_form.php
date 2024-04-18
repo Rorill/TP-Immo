a2ae317eb9ba8ef66da133ae04e62d2f9a72e355
Guillaume Herold <guillaumeherold@gmail.com>
2024-04-18 12:02:18 +0200

    optimisation

--- partials/Listings/_form.php
+++ partials/Listings/_form.php
@@ -1,3 +1,15 @@
+<?php 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $ListingType = trim($_POST["ListingType"]);
+  $title = trim($_POST["title"]);
+  $mail = trim($_POST["mail"]);
+  $price = trim($_POST["Price"]);
+  $location = trim($_POST['Location']);
+  $message = trim($_POST["message"]);
+ 
+}
+?> 
+
 <form action="" method="post" class="newListingForm">
 
 <label class="label" for="ListingType">Listing Type :</label>
@@ -5,66 +17,50 @@
 <option value="Rent">Rent</option>
 <option value="Sale">Sale</option>
 </select>
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && empty($ListingType)): ?>
+<p class="errors"> please choose a Listing Type </p>
+<?php endif ?>
 
-<label class="label" for="name">Your name :</label>
-<input class="textInput" type="text" name="name"></input>
 
-<label class="label" for="mail">Your email :</label>
+<label class="label" for="title">Title :</label>
+<input class="textInput" type="text" name="title"></input>
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && empty($title)): ?>
+<p class="errors"> please enter a title </p>
+<?php endif ?>
+
+
+<label class="label" for="mail">contact mail :</label>
 <input class="textInput" type="mail" name="mail"></input>
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && empty($mail)): ?>
+<p class="errors"> please enter a contact mail </p>
+<?php endif ?>
+
 
 <label class="label" for="Price">Price :</label>
 <input class="textInput" type="number" name="Price">
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && empty($price)): ?>
+<p class="errors"> please enter a title </p>
+<?php endif ?>
+
 
 <label class="label" for="Location">Localization of your property :</label>
 <input class="textInput" type="text" name="Location" id="Location">
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && empty($location)): ?>
+<p class="errors"> please enter a location </p>
+<?php endif ?>
 
-<label class="label" for="description">Description :</label>
-<input class="textInput description" type="text" name="description"></input>
+
+<label class="label" for="message">Description :</label>
+<input class="textInput description" type="text" name="message"></input>
+<?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && empty($message)): ?>
+<p class="errors"> please enter a description </p>
+<?php endif ?>
+
 
 
 <button class="sendListing" type="submit">Create Listing</button>
 </form>
 
-<?php 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $ListingType = trim($_POST["ListingType"]);
-  $name = trim($_POST["name"]);
-  $mail = trim($_POST["mail"]);
-  $price = trim($_POST["Price"]);
-  $location = trim($_POST['Location']);
-  $message = trim($_POST["message"]);
 
-  $errors = [];
 
-  if (empty($ListingType)) {
-    $errors[] = "Please chose your type of listing";
-  }
-  if (empty($name)) {
-    $errors[] = "Please enter a name";
-  }
-  if (empty($mail)) {
-    $errors[] = "Please enter a mail";
-  }
-  if (empty($price)) {
-    $errors[] = "Please enter a price";
-  }
-  if (empty($location)) {
-    $errors[] = "Please enter a location";
-  }
-  if (empty($message)) {
-    $errors[] = "Please write a description";
-  }
- 
-}
-?> 
 
-<?php if (!empty($errors)) : ?>
-    <div class="errorContainer">
-    <h2 class="errorMessage">Merci de corriger les erreurs suivantes :</h2>
-    <ul class="errors">
-      <?php foreach ($errors as $error) : ?>
-        <li class="formError"><?= $error ?></li>
-        <?php endforeach; ?>
-    </ul>
-      </div>
-  <?php endif; ?>
